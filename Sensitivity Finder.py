d4073679eaf1a330dffcae34dcf84ad59b9663c0
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-10-31 05:40:13 +0700

    Update Sensitivity Finder.py

--- Sensitivity Finder.py
+++ Sensitivity Finder.py
@@ -1,12 +1,17 @@
 lastValue = 0
 currrentValue = 100.00
 sensitive = 100.00
-counter = 0
+
 print(f'{currrentValue:.2f}?')
 
 while True :
     word = input()
-    currrentValue = abs(sensitive - lastValue)
+    
+    if (sensitive - lastValue < 0) :
+        currrentValue = lastValue - sensitive
+    else :
+        currrentValue = sensitive - lastValue
+
     if (word == "S") :
         sensitive = (currrentValue * 2) + lastValue
     elif (word == 'F') :
@@ -14,4 +19,5 @@ while True :
     elif (word == "D") :
         break
     lastValue = currrentValue
+
     print(F'{sensitive:.2f}?')
