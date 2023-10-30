76fdc3181aecfc797bedb971e50ae2f6d3b972e0
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2023-10-30 14:01:20 +0700

    .

---
+++ [Dict] InJoker.py
@@ -0,0 +1,20 @@
+skillMarble = {
+    "QQQ" : "COLD SNAP",
+    "QQW" : "GHOST WALK",
+    "QQE" : "ICE WALL",
+    "WWW" : "E.M.P",
+    "WWQ" : "TORNADO",
+    "WWE" : "ALACRITY" ,
+    "EEE" : "SUN STRIKE",
+    "EEQ" : "FORGE SPIRIT",
+    "EEW" : "CHAOS METEOR",
+    "QWE" : "DEFEANING BLAST"
+}
+
+skillList = []
+
+skillInput = input()
+
+for i in skillInput :
+    skillList.append(i)
+print(skillList)
