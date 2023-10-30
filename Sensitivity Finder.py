67132272cff8e71a8016f15017831fd2e97cc1c7
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-10-31 02:05:59 +0700

    Update Sensitivity Finder.py

--- Sensitivity Finder.py
+++ Sensitivity Finder.py
@@ -1,26 +1,17 @@
-minimum = 50.00
-maximum = 100.00
-
+lastValue = 0
+currrentValue = 100.00
+sensitive = 100.00
 counter = 0
-lastword = ''
-print('%.2f' % maximum + "?")
-while 1 :
+print(f'{currrentValue:.2f}?')
+
+while True :
     word = input()
-    counter += 1
-
-    if (word != "D") :
-        if (word == "F") :
-            maximum -= minimum
-            lastword = 'F'
-            minimum /= 2
-        elif (word == "S") :
-            minimum *= 2
-            maximum += minimum
-            lastword = 'S'
-        print('%.2f' % maximum + "?")
-    else :
+    currrentValue = abs(sensitive - lastValue)
+    if (word == "S") :
+        sensitive = (currrentValue * 2) + lastValue
+    elif (word == 'F') :
+        sensitive = (currrentValue / 2) + lastValue
+    elif (word == "D") :
         break
-    if (lastword == word) :
-        minimum /= 2
-
-print('Your sensitivity is ' + '%.2f' % maximum + ".")
+    lastValue = currrentValue
+    print(F'{sensitive:.2f}?')
