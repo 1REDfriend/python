2718d22e3029ad8fe27f7bf8d90f36ecb7b47295
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2023-11-02 17:29:05 +0700

    .

--- OTP.py
+++ OTP.py
@@ -1,33 +1,41 @@
-def is_valid_otp(otp):
-    otp_str = str(otp)
-    
-    if len(otp_str) % 2 == 0:
-        # OTP มีจำนวนหลักคู่ (4, 6, 8)
-        digit_count = {}
-        
-        for digit in otp_str:
-            if digit in digit_count:
-                digit_count[digit] += 1
-            else:
-                digit_count[digit] = 1
-        
-        # ตรวจสอบเงื่อนไขตามความยาวของ OTP
-        if len(otp_str) == 4 and 1 in digit_count.values() and 2 in digit_count.values():
-            return "Valid"
-        elif len(otp_str) == 6 and (2 in digit_count.values() or 1 in digit_count.values()) and 4 in digit_count.values():
-            return "Valid"
-        elif len(otp_str) == 8 and (3 in digit_count.values() or 2 in digit_count.values()) and 6 in digit_count.values():
-            return "Valid"
-        else:
-            return "Invalid"
-    else:
-        # OTP มีจำนวนหลักคี่ ไม่ถูกต้อง
-        return "Invalid"
-
-# รับ OTP จากผู้ใช้เรื่อยๆ จนกว่าจะรับค่า 0
 while True:
-    otp = int(input())
-    if otp == 0:
+    otp = input()
+    
+    if otp == "0":
         break
-    result = is_valid_otp(otp)
-    print(result)
+    
+    if len(otp) == 4:
+        duplicate_count = 0  # ตัวแปรเพื่อเก็บจำนวนตัวที่ซ้ำกัน
+        for i in range(len(otp)):
+            for j in range(i+1, len(otp)):
+                if otp[i] == otp[j]:
+                    duplicate_count += 1
+
+        if duplicate_count == 1:
+            print(f"Valid")
+        else :
+            print("Invalid")
+    elif len(otp) == 6 :
+        duplicate_count = 0  # ตัวแปรเพื่อเก็บจำนวนตัวที่ซ้ำกัน
+        for i in range(len(otp)):
+            for j in range(i+1, len(otp)):
+                if otp[i] == otp[j]:
+                    duplicate_count += 1
+
+        
+        if duplicate_count >= 2:
+            print(f"Valid")
+        else :
+            print("Invalid")
+    elif len(otp) == 8:
+        duplicate_count = 0  # ตัวแปรเพื่อเก็บจำนวนตัวที่ซ้ำกัน
+        for i in range(len(otp)):
+            for j in range(i+1, len(otp)):
+                if otp[i] == otp[j]:
+                    duplicate_count += 1
+
+        if duplicate_count >= 2:
+            print(f"Valid")
+        else :
+            print(f"Invalid")
+
