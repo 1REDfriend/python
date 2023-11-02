13e22cdf549250663d8a574611395c2f1a4d7cac
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-03 00:00:24 +0700

    .

--- OTP.py
+++ OTP.py
@@ -27,15 +27,4 @@ while True:
             print(f"Valid")
         else :
             print("Invalid")
-    elif len(otp) == 8:
-        duplicate_count = 0  # ตัวแปรเพื่อเก็บจำนวนตัวที่ซ้ำกัน
-        for i in range(len(otp)):
-            for j in range(i+1, len(otp)):
-                if otp[i] == otp[j]:
-                    duplicate_count += 1
-
-        if duplicate_count >= 2:
-            print(f"Valid")
-        else :
-            print(f"Invalid")
-
+    
