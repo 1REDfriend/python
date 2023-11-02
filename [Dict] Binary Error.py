6406a3f25ba5ca0ff664fcbc73649f1e9c787fc1
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-03 00:56:24 +0700

    .

--- [Dict] Binary Error.py
+++ [Dict] Binary Error.py
@@ -1,21 +1,35 @@
-
 my_list = []
 
 while True:
     text = input()
     if text.lower() != "end":
+        try :
+            text = int(text)
+        except ValueError :
+            break
         found = False
         for item in my_list:
             if item[0] == text:
                 item[1] += 1
                 found = True
-                break
         if not found:
             my_list.append([text, 1])
     else:
         break
+stacklist = []
 
 for i in my_list :
     num = int(i[0]) + i[1]
-    binary = "{0:b}".format(num)
-    print(f'{i[0]}-->{binary}')
+    ขยะ = ''
+    
+    if num == 0:
+        ขยะ = '0'
+    else:
+        while num > 0:
+            ขยะ = str(num % 2) + ขยะ
+            num //= 2
+    stacklist.append([i[0],"-->",ขยะ])
+
+stacklist.sort(key=lambda x: int(x[0]))
+for i in stacklist :
+    print(f'{i[0]}{i[1]}{i[2]}')
