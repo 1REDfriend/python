e34c764c4248e818851ff065a7249e68542a9252
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2023-11-02 08:50:35 +0700

    .

--- [ List ] หนังสือเล่มที่ II.py
+++ [ List ] หนังสือเล่มที่ II.py
@@ -4,7 +4,7 @@ couter = 1
 
 while True:
     bookcode = input()
-    if bookcode == "end":
+    if bookcode.lower() == "end":
         break
     else :
         books.append(bookcode)
@@ -13,6 +13,14 @@ while True:
 for i,v in enumerate(books) :
     # จะหาว่ามีหนังสืออยู่ใน shelv หรือป่าว ถ้าไม่ให้เพิ่ม books, i , 1
     # แต่ถ้ามี ให้ บวก จำนวนเพิ่ม
+    if i == 0 :
+        shelv.append((v , i+1 , 1))
+    else :
+        try:
+            if (v in shelv) :
+                print(v)
+        except ValueError :
+            shelv.append((v , i , 1))
 
 for i in shelv :
     print(i[0],i[1],i[2])
