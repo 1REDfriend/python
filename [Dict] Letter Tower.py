db9d2910133f7254f8807ae640a6ddab315917e4
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2023-11-03 18:06:56 +0700

    .

--- [Dict] Letter Tower.py
+++ [Dict] Letter Tower.py
@@ -1,4 +1,19 @@
-name = input()
-dictname = {}
-for i in len(name) :
-    dictname.fromkeys(i, 1)
+tower = {}
+
+text = input()
+
+for i in text:
+    Found = False
+    for key in tower:
+        if key == i:
+            tower[key] += 1
+            Found = True
+            break
+    if not Found:
+        tower[i] = 1
+
+tower = dict(sorted(tower.items(), key=lambda item: item[1], reverse=True))
+print("_________")
+for i,v in tower.items() :
+    print(f'|{i} <-> {v}|')
+print("*********")
