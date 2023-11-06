d6132df94c5a97a6575b0000bf10e99758007504
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2023-11-06 16:40:29 +0700

    ..

--- Travel to MBK.py
+++ Travel to MBK.py
@@ -9,24 +9,27 @@ money = input()
 try :
     if money.isdigit() :
         money = int(money)
-except ValueError :
-    money = int(money)
-traveloga = []
+        traveloga = []
 
-for i,v in travellist.items() :
-    if i <= money :
-        money = money - i
-        traveloga.append(v)
-        # print(money)
+        for i,v in travellist.items() :
+            if i <= money :
+                money = money - i
+                traveloga.append(v)
+                # print(money)
 
-# print(traveloga)
-if not traveloga :
-    print("stay home")
-else :
-    for i in traveloga :
-        if i == "Song Thaeo" or i == "BTS":
-            print(f'{i}\nwalking')
-            break
+        # print(traveloga)
+        if not traveloga :
+            print("stay home")
         else :
-            print(f'{i}\nno walking')
-            break
+            for i in traveloga :
+                if i == "Song Thaeo" or i == "BTS":
+                    print(f'{i}\nwalking')
+                    break
+                else :
+                    print(f'{i}\nno walking')
+                    break
+    else :
+        print("stay home")
+except ValueError :
+    print("stay home")
+
