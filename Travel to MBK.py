7254e80e76e5ef4cf79ec409c077eaebcdd3eab3
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-08 00:34:09 +0700

    MOTHER

--- Travel to MBK.py
+++ Travel to MBK.py
@@ -1,35 +1,20 @@
-travellist = {
-    50 : "Taxi",
-    40 : "BTS",
-    25 : "Motocycle",
-    8 : "Song Thaeo"
-}
-
-money = input()
 try :
-    if money.isdigit() :
-        money = int(money)
-        traveloga = []
+    budget = float(input())
 
-        for i,v in travellist.items() :
-            if i <= money :
-                money = money - i
-                traveloga.append(v)
-                # print(money)
-
-        # print(traveloga)
-        if not traveloga :
-            print("stay home")
-        else :
-            for i in traveloga :
-                if i == "Song Thaeo" or i == "BTS":
-                    print(f'{i}\nwalking')
-                    break
-                else :
-                    print(f'{i}\nno walking')
-                    break
-    else :
+    if budget < 8:
         print("stay home")
-except ValueError :
+    else:
+        if budget >= 50:
+            print("Taxi")
+            print("no walking")
+        elif budget >= 40:
+            print("BTS")
+            print("walking")
+        elif budget >= 25:
+            print("Motorcycle")
+            print("no walking")
+        else:
+            print("Song Thaeo")
+            print("walking")
+except :
     print("stay home")
-
