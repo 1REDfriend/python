229ab49c0d9743fd5f1a52ade33ec55fde07fb3a
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2023-11-08 09:36:46 +0700

    for loop wtf

--- My Final Magazine.py
+++ My Final Magazine.py
@@ -3,16 +3,11 @@ bullet = int(input())
 EnemyHealth = float(input())
 bulletDM = float(input())
 
-if (bullet != 0 and bulletDM != 0 and EnemyHealth != 0) :
-    if (EnemyHealth // bulletDM) <= bullet :
-        bulletremains = int(bullet - (EnemyHealth // bulletDM))
-        print(f'dead : {int(bulletremains)} bullet remain')
+for i in range(bullet) :
+    if EnemyHealth > 0 and bullet > 0:
+        EnemyHealth -= bulletDM
+        bullet -= 1
     else :
-        EnemyHealthremains = EnemyHealth - (bullet * bulletDM)
-        print(f'alive : {EnemyHealthremains} health')
-elif (bullet == 0 ) :
-    print(f'alive : {EnemyHealth} health')
-elif (EnemyHealth == 0) :
-    print(f'dead : {int(bullet)} bullet remain')
-else :
-    print(f'alive : {EnemyHealth} health')
+        break
+
+if ()
