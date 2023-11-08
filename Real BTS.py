e9adb1a1e93d4fad4d46eadafd7b1d0e9bf58b2b
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-08 18:31:31 +0700

    ?

--- Real BTS.py
+++ Real BTS.py
@@ -1,14 +1,51 @@
 station1 = input()
 station2 = input()
 
+price = 0
+
 BTS = {
-    "16-44" : ['N8', 'N7', 'N6', 'N5', 'N4', 'N3', 'N2', 'N1', '0', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9' , 'W1', '0', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6' , 'S7' , 'S8'],
-    "15" : ['E10', 'E11', 'E12', 'E13', 'E14' , 'S9', 'S10', 'S11', 'S12'],
-    "Free" : ['N9', 'N10', 'N11', 'N12', 'N13', 'N14', 'N15', 'N16', 'N17', 'N18', 'N19', 'N20', 'N21', 'N22', 'N23', 'N24' , 'E15', 'E16', 'E17', 'E18', 'E19', 'E20', 'E21', 'E22', 'E23']
+    "N" : [1,24],
+    "E" : [1,23],
+    "S" : [1,12],
+    "W" : [1,1]
 }
+
+btsprice = [17,25,28,32,35,40,43,47]
 
 if station1 == "N6" or station2 == "N6" :
     print("Error")
 else :
-    
-    
+    station1list = [i for i in station1]
+    station2list = [i for i in station2]
+    station1code = station1list[0]
+    station2code = station2list[0]
+    station1num = int(''.join([station1list[i] for i in range(1,len(station1list))]))
+    station2num = int(''.join([station2list[i] for i in range(1,len(station2list))]))
+
+    for i,v in BTS.items() :
+        if i == station1code :
+            if station1code == station2code :
+                maxnum = max(station1num,station2num)
+                minnum = min(station1num,station2num)
+                counter = 0
+
+                for num in range(minnum , maxnum + 1) :
+                    if station1code == "N" :
+                        if num not in range(8,25) :
+                            counter += 1
+                    elif station1code == "E" :
+                        if num not in range(14,24) :
+                            counter += 1
+
+                if counter != -1 :
+                    if counter <= len(btsprice) :
+                        if counter != 0 :
+                            price = btsprice[counter - 1]
+                        else :
+                            price = btsprice[counter]
+                    else :
+                        price = btsprice[len(btsprice) - 1]
+
+                
+                
+                    
