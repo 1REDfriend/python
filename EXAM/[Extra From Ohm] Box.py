a58479037e67c2f5a375b8fb7443b035e40e5476
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-14 00:17:37 +0700

    .

--- EXAM/[Extra From Ohm] Box.py
+++ EXAM/[Extra From Ohm] Box.py
@@ -6,16 +6,23 @@ startdict = {
 
 }
 
+# make colum
+
 for i in range(boxY) :
     found = False
     i+=1
-    for x,n in startdict.items() :
-        x+=1
-        print(x,i)
-        if x is i :
-            found = True
-            startdict[i] += "*"
-    if not found :
-        startdict[i] = list("*")
+    startdict[i] = list("*")
 
+# push star in dict
+
+current_stardictY = boxY 
+def next_stardictY () :
+    if current_stardictY < 0 :
+        current_stardictY = boxY
+    else :
+        current_stardictY -= 1
+    return current_stardictY
+
+for i in range(starfield) :
+    startdict[next_stardictY()].append("*")
 print(startdict)
